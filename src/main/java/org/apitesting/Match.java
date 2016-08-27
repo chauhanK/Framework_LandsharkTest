3cbe3b3118a83660a7235e5cd9277293aae84980
karan chauhan <karan chauhan>
2016-08-27 18:16:00 +0530

    Read data from Excel

--- src/main/java/org/apitesting/Match.java
+++ src/main/java/org/apitesting/Match.java
@@ -1,6 +1,9 @@
 package org.apitesting;
 
 import java.net.URL;
+import java.util.ArrayList;
+
+import static org.apitesting.Util.*;
 
 public class Match {
 
@@ -11,6 +14,16 @@ public class Match {
 		HttpConnector server = new HttpConnector(url);
 		String response = server.getResponse();
 		server.printResponse(response);
+		
+		ExcelReader reader = new ExcelReader();
+		String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\";
+		ArrayList<String> cells = new ArrayList<String>();
+		cells.add("A1");
+		cells.add("B1");
+		ArrayList<String> values =  reader.readExcel(filePath, "haha.xlsx", "Sheet1", cells);
+		
+		printExcel(values);
+		
 	}
 
 }
