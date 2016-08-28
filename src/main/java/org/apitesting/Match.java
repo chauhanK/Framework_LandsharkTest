042eeda069a694d7ac7dd56e33799e2fb237486d
chauhanK <k.c.thakur@outlook.com>
2016-08-28 23:22:09 +0530

    refactored code

--- src/main/java/org/apitesting/Match.java
+++ src/main/java/org/apitesting/Match.java
@@ -10,37 +10,32 @@ import java.util.ArrayList;
 public class Match {
 
 	public static void main(String[] args) throws Exception {
-		URL url = new URL(
-				"http://maps.googleapis.com/maps/api/geocode/json?address=chicago&sensor=false&#8221;");
+		ExcelReader reader = new ExcelReader();
+		String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\";
+		ArrayList<String> cells = new ArrayList<String>();
+		cells.add("A1");
+		ArrayList<String> expectedvalues =  reader.readExcel(filePath, "haha.xlsx", "Sheet1", cells);
+		
+		ArrayList<String> Url = new ArrayList<String>();
+		Url.add("A1");
+		ArrayList<String> expectedUrl =  reader.readExcel(filePath, "haha.xlsx", "Sheet2", Url);
+		
+		URL url = new URL(expectedUrl.get(0));
 
 		HttpConnector server = new HttpConnector(url);
 		String response = server.getResponse();
 		server.printResponse(response);
 		
-		ExcelReader reader = new ExcelReader();
-		String filePath = System.getProperty("user.dir")+"\\src\\main\\java\\";
-		ArrayList<String> cells = new ArrayList<String>();
-		cells.add("A1");
-		cells.add("B1");
-		ArrayList<String> expectedvalues =  reader.readExcel(filePath, "haha.xlsx", "Sheet1", cells);
 		
 		printExcel(expectedvalues);
 		
 		JSONObject jsonResponse = new JSONObject(response);
 		
-		 JSONArray result = jsonResponse.getJSONArray("results");
-		 JSONObject result1 =  result.getJSONObject(0);
-		 
-		 String address = result1.getString("formatted_address");
-		 
-			System.out.println("status : " + address);		 
-		 
-		
-		/*for(String value : expectedvalues)
+		for(String value : expectedvalues)
 		{
-			String responseCode = obj.getString(value);
+			String responseCode = jsonResponse.getString(value);
 			System.out.println("status : " + responseCode);
-		}*/
+		}
 		
 
 	}
